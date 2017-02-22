00d053be04cc493aac884ddf22fdca7e8457e3ad
skk4 <skk_4@163.com>
2017-02-22 15:50:52 +0800

    time

--- src/test/java/step_def/yoyaloginstepfs.java
+++ src/test/java/step_def/yoyaloginstepfs.java
@@ -17,7 +17,7 @@ public class yoyaloginstepfs {
 	    //throw new PendingException();
 		driver = new ChromeDriver();
 		driver.get("http://www.yoya.com/");
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 	}
 
 	@Then("^检查yoya页面是否打开$")
@@ -25,45 +25,50 @@ public class yoyaloginstepfs {
 	    // Write code here that turns the phrase above into concrete actions
 	    //throw new PendingException();
 		Assert.assertEquals("优芽网 帮您更优秀- www.yoya.com", driver.getTitle());
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 	}
 
 	@Given("^点击立刻登录按键$")
 	public void 点击立刻登录按键() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	    //throw new PendingException();
+		Thread.sleep(5000);
 	    driver.findElement(By.id("_yoya_login")).click();
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 	}
 
 	@Then("^检查登录页面是否打开$")
 	public void 检查登录页面是否打开() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	    //throw new PendingException();
+		Thread.sleep(5000);
 		if(driver.findElement(By.id("_pop_user_login")).isEnabled()) { 
 	         System.out.println("Test 1 Pass"); 
 	      } else { 
 	         System.out.println("Test 1 Fail"); 
 	      } 
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 	}
 
 	@When("^输入用户名 \"([^\"]*)\" 密码 \"([^\"]*)\"$")
 	public void 输入用户名_密码(String username, String password) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	   // throw new PendingException();
+		Thread.sleep(5000);
 		driver.findElement(By.id("_pop_user_name")).sendKeys(username);
 		driver.findElement(By.id("_pop_password")).sendKeys(password);
 		driver.findElement(By.id("_pop_user_login")).click();
-		Thread.sleep(3000);
+		Thread.sleep(5000);
 	}
 
 	@Then("^检查是否登录成功 \"([^\"]*)\"$")
 	public void 检查是否登录成功(String exceptText) throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	    //throw new PendingException();
+		Thread.sleep(5000);
 		Assert.assertEquals(exceptText, driver.getTitle());
 		driver.close();
+		Thread.sleep(5000);
 		
 	}
 
